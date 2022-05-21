d124ff1322e85a4c6e95ec2e948814ca691736e8
Fabiany Lopes <53617138+fabianylopes@users.noreply.github.com>
2022-05-20 22:08:10 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,6 +25,8 @@ const ContainerTop = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    position: fixed;
+    top: 0;
 
     background-color: #0000CD;
 
