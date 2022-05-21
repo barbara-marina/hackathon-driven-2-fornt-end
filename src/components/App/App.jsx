1e8f016f3fe8bf85dfec94d0c1993498de49fd99
Fabiany Lopes <53617138+fabianylopes@users.noreply.github.com>
2022-05-20 21:05:25 -0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Home from "../Home/Home.jsx";
-
+import SignIn from "../Auth/SignIn";
+import SignUp from "../Auth/SignUp";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
+                <Route path="/sign-in" element={<SignIn/>}></Route>
+                <Route path="/sign-up" element={<SignUp/>}></Route>
             </Routes>
         </BrowserRouter>
     );
